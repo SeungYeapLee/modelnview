2ac98410674b35c74d4436f61d7a05f07716b3f0
existmaster <existmaster@gmail.com>
2018-07-20 12:20:58 +0900

    2nd

--- src/main/java/com/example/modelnview/HelloController.java
+++ src/main/java/com/example/modelnview/HelloController.java
@@ -2,7 +2,10 @@ package com.example.modelnview;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,11 +14,19 @@ public class HelloController {
     @Autowired
     MydataRepository repository;
 
-    @RequestMapping("/")
-    public ModelAndView index(ModelAndView mav) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index(@ModelAttribute("formModel") MyData myData, ModelAndView mav) {
         mav.setViewName("index");
+        mav.addObject("msg", "This is Sample Content.");
         Iterable<MyData> list = repository.findAll();
-        mav.addObject("data", list);
+        mav.addObject("datalist", list);
         return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public ModelAndView form(@ModelAttribute("formModel") MyData myData, ModelAndView mav) {
+        repository.saveAndFlush(myData);
+        return new ModelAndView("redirect:/");
     }
 }
